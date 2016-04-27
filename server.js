6ac5013c0e9e0e765bb1e2fd07a6a9c954dd124d
Donovan Hiland <donovan.hiland@gmail.com>
2016-04-27 11:58:22 -0600

    initial

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var mongoose = require('mongoose');
+var Sighting = require('./Sighting');
 
 var app = express();
 app.use(bodyParser.json());
+mongoose.set("debug", true);
+mongoose.connect('mongodb://localhost/birds-mongoose');
+
+mongoose.connection.once('open', function() {
+  console.log("connected to MongoDB");
+});
 
 var port = 3000;
 
 app.post('/api/sighting', function(req, res) {
-  console.log('POST sighting');
-  res.end();
+  var sighting = new Sighting(req.body);
+  sighting.save(function(err, s) {
+    if(err) {
+      res.status(500).send(err);
+    }
+    else {
+      res.send(s);
+    }
+  });
 });
- 
+
 app.get('/api/sighting', function(req, res) {
   console.log('GET sighting');
   res.end();
 });
- 
+
 app.delete('/api/sighting', function(req, res) {
   console.log('DELETE sighting');
   res.end();
 });
- 
+
 app.put('/api/sighting', function(req, res) {
   console.log('PUT sighting');
   res.end();
